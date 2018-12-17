a5d1a3a4d065127146748dd1d454a8137caca9d6
linqiang69672187 <69672187@qq.com>
2018-12-17 11:12:09 +0800

    1

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.urls import path,include
 from . import views
-
+app_name='blog'
+#2.0开始namespace设置app_name
 urlpatterns=[
    # url(r'^$',views.blog_title,name="blog_title")
   #url(r'(?P<article_id>\d)/$’, views. blog_article, name=” blog_detail”),
-    path(r'', views.blog_title),
-    path('<int:acticle_id>/',views.blog_acticle ),
+    path(r'', views.blog_title,name='blog_title'),
+    path('<int:acticle_id>/',views.blog_acticle, name="blog_detail" ),
 ]
